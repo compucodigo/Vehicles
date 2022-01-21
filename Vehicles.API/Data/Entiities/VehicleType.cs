e6f4a8605803ce3f177ec61a1e54169f954108ba
Juan Pérez <jmph81@gmail.com>
2022-01-21 10:20:17 -0400

    Agregar tipo de vehiculos y crear BD

---
+++ Vehicles.API/Data/Entiities/VehicleType.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Vehicles.API.Data.Entiities
+{
+    public class VehicleType
+    {
+        public int Id { get; set; }
+        [Display(Name ="Tipo de Vehiculo")]
+        [MaxLength(50, ErrorMessage ="El campo {0} tiene un maximo de {1} caracteres")]
+        [Required(ErrorMessage ="El campo {0} es requerido")]
+        public string Description { get; set; }
+    }
+}
